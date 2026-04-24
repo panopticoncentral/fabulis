6b01d3d7a53b6ef50f3aad4daebc44f8e9c9455f
Paul Vick <paul@panopticoncentral.net>
2026-04-24 10:54:50 -0700

    Extend VaultService with activity tracking and auto-lock timeout

--- src/Fabulis.Server/Data/VaultService.cs
+++ src/Fabulis.Server/Data/VaultService.cs
@@ -4,16 +4,33 @@ public class VaultService
 {
     public bool IsUnlocked { get; private set; }
     public string? Password { get; private set; }
+    public DateTime LastActivityAt { get; private set; }
+    public TimeSpan? AutoLockTimeout { get; private set; }
 
     public void Unlock(string password)
     {
         Password = password;
         IsUnlocked = true;
+        LastActivityAt = DateTime.UtcNow;
     }
 
     public void Lock()
     {
         Password = null;
         IsUnlocked = false;
+        AutoLockTimeout = null;
+    }
+
+    public void RecordActivity()
+    {
+        if (IsUnlocked)
+            LastActivityAt = DateTime.UtcNow;
+    }
+
+    public void ConfigureAutoLock(int? minutes)
+    {
+        AutoLockTimeout = minutes is int m && m > 0
+            ? TimeSpan.FromMinutes(m)
+            : null;
     }
 }
