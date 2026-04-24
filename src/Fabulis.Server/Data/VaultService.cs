14c7e9549c7c0276203ce7cc297edb818f9a7f6f
Paul Vick <paul@panopticoncentral.net>
2026-04-24 10:57:17 -0700

    Use Interlocked/Volatile for thread-safe vault activity tracking

--- src/Fabulis.Server/Data/VaultService.cs
+++ src/Fabulis.Server/Data/VaultService.cs
@@ -2,35 +2,47 @@ namespace Fabulis.Server.Data;
 
 public class VaultService
 {
+    private long _lastActivityTicks;
+    private int _autoLockMinutes;
+
     public bool IsUnlocked { get; private set; }
     public string? Password { get; private set; }
-    public DateTime LastActivityAt { get; private set; }
-    public TimeSpan? AutoLockTimeout { get; private set; }
+
+    public DateTime LastActivityAt =>
+        new DateTime(Interlocked.Read(ref _lastActivityTicks), DateTimeKind.Utc);
+
+    public TimeSpan? AutoLockTimeout
+    {
+        get
+        {
+            var minutes = Volatile.Read(ref _autoLockMinutes);
+            return minutes > 0 ? TimeSpan.FromMinutes(minutes) : null;
+        }
+    }
 
     public void Unlock(string password)
     {
         Password = password;
         IsUnlocked = true;
-        LastActivityAt = DateTime.UtcNow;
+        Interlocked.Exchange(ref _lastActivityTicks, DateTime.UtcNow.Ticks);
     }
 
     public void Lock()
     {
         Password = null;
         IsUnlocked = false;
-        AutoLockTimeout = null;
+        Volatile.Write(ref _autoLockMinutes, 0);
     }
 
     public void RecordActivity()
     {
         if (IsUnlocked)
-            LastActivityAt = DateTime.UtcNow;
+            Interlocked.Exchange(ref _lastActivityTicks, DateTime.UtcNow.Ticks);
     }
 
     public void ConfigureAutoLock(int? minutes)
     {
-        AutoLockTimeout = minutes is int m && m > 0
-            ? TimeSpan.FromMinutes(m)
-            : null;
+        var value = minutes is int m && m > 0 ? m : 0;
+        Volatile.Write(ref _autoLockMinutes, value);
     }
 }
